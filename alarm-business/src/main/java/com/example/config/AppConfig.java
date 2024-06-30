764df9f35e901d7a03a9219c34515afeacd524fe
chang xu <1263919792@qq.com>
2024-07-01 00:27:37 +0800

    1

---
+++ alarm-business/src/main/java/com/example/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.example.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "a")
+@PropertySource("classpath:application-dev.yml") // 指定其他JAR包中的配置文件路径
+@Data
+public class AppConfig {
+
+    private String b;
+}
